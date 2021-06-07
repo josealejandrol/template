ac0ed42606d8da741bc761cd9b70a6d3d5e60419
José Alejandro <josealejandrolzr@gmail.com>
2021-06-07 18:04:57 -0500

    avance

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,20 @@
 export const Testimonials = () => {
   return (
     <div className="testimonials__content">
-      <h1>Testimonials</h1>
+      <div className="testimonials__content-box">
+        <div className="testimonials__content-top">
+          <h1>Testimonials</h1>
+          <p>Quisque sodales metus et mauris imperdiet non volutpat.</p>
+          <p className="bold">
+            Nulla turpis velit, imperdiet vel viverra ut, tristique sit amet
+            felis.
+          </p>
+        </div>
+        <div className="testimonials__content-row">
+          <div className="testimonials__content-row-column1"></div>
+          <div className="testimonials__content-row-column2"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
